7f839cb8c4c021d646b75abeb720d6a37e43d927
nichorpl-lang <nichorpl@gmail.com>
2026-08-05 12:38:58 +0700

    Add files via upload

---
+++ BAB_3.py
@@ -0,0 +1,21 @@
+Nama = "M.Febriyan Nicho Nur Hidayat"
+Umur = "15"
+Tinggi = "161.7cm"
+Lulus = "True" 
+print ("Nama saya adalah:", Nama)
+print ("Umur saya:", Umur)
+print ("Tinggi saya:", Tinggi)
+print ("Lulus:", Lulus)
+print ()
+print ()
+Nama = "Sepatu Lari"
+Stok = (10 + 5)
+Harga = 249.5
+Tersedia = "True"
+print ("Nama Produk:", Nama)
+print ("Stok Produk:", Stok)
+print ("Harga Produk ($):", Harga + 56)
+print ("Tersedia:", Tersedia)
+
+
+ 
