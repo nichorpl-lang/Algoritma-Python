64481120b7340cca716cea9d854bde26890b41b6
nichorpl-lang <nichorpl@gmail.com>
2026-08-05 08:15:07 +0700

    Add files via upload

---
+++ program.py
@@ -0,0 +1,32 @@
+# Program Pertamaku
+# Nama: M.Febriyan Nicho Nur Hidayat
+# Kelas: X RPL 2
+
+print ("====================================")
+print ("      SELAMAT DATANG DI PYTHON      ")
+print ("====================================")
+print ()
+print ("Program Ini Dibuat Oleh:", "M.Febriyan Nicho Nur Hidayat")
+print ("Sekolah: SMK TJP TUBAN")
+print ("Tahun :", 2026)
+print ()
+print (10 + 5)
+print (20 - 8)
+print (4*7)
+print (15/4)
+print (15//4)
+print (15%4)
+print (2**8)
+print ()
+a=24
+b=7
+c=8
+print ("penjumlahan:", a + b)
+print ("pengurangan:", a - b)
+print ("perkalian:", b * c)
+print ("pembagian:", a / c)
+print ()
+
+
+
+
